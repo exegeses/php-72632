edd219e1c673c9130702b5eb56dc2828d3123b6b
Marcos <exegeses@gmail.com>
2024-11-02 13:06:08 -0300

    Teoría y práctica inicial de funciones personalizadas

--- curso/clases3/funciones.php
+++ curso/clases3/funciones.php
@@ -1,12 +1,36 @@
 <?php
 include '../layouts/header.php';
+
+// declaración
+    function saludar( string $nombre ) : string
+    {
+        return 'hola '. $nombre. '<br>';
+    }
+
+    function sumar( int|float $numero1, int|float $numero2 ) : int|float
+    {
+        return $numero1 + $numero2;
+    }
+
 ?>
     <main class="container py-3">
-        <h1> funciones personalizadas</h1>
+        <h1>funciones personalizadas</h1>
 
         <section class="shadow alert my-3">
-            contenido de la página
+<?php
+        //Invocamos la función saludar()
+        echo saludar( 'Rick' );
+        echo saludar( 'Morty' );
+        echo saludar( 'Summer' );
+?>
         </section>
+
+        <section class="shadow alert my-3">
+<?php
+        echo sumar(4.25, 5);
+?>
+        </section>
+
 
     </main>
 <?php
