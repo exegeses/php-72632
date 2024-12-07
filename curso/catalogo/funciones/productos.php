5f1110ed16822dbe1682d08f332c2a0f3fe6d8fb
Marcos <exegeses@gmail.com>
2024-12-07 13:04:22 -0300

    Aplicación de alta de un producto

--- curso/catalogo/funciones/productos.php
+++ curso/catalogo/funciones/productos.php
@@ -28,6 +28,65 @@
         return mysqli_query($link, $sql);
     }
 
+function subirImagen() : string
+{
+    // si no se envió imagen
+    $nombre = 'noDisponible.svg';
+
+    // IMAGEN ENVIADA
+    if( $_FILES['prdImagen']['error']==0 ){
+        $tmp = $_FILES['prdImagen']['tmp_name'];
+        $dir = 'productos/';
+        // renombrado de archivo
+        $ext = pathinfo($_FILES['prdImagen']['name'], PATHINFO_EXTENSION);
+        $nombre = time().'.'.$ext;
+        // Copiar archivo enviado a carpeta "productos/"
+        move_uploaded_file($tmp, $dir.$nombre);
+    }
+    return $nombre;
+}
+
+function agregarProducto() : bool
+{
+    // capturamos datos enviador por el form
+    $prdNombre = $_POST['prdNombre'];
+    $prdPrecio = $_POST['prdPrecio'];
+    $idMarca = $_POST['idMarca'];
+    $idCategoria = $_POST['idCategoria'];
+    $prdDescripcion = $_POST['prdDescripcion'];
+    //subir imagen *
+    $prdImagen = subirImagen();
+
+    $sql = "INSERT INTO productos
+        (prdNombre, prdPrecio, idMarca, idCategoria, prdDescripcion, prdImagen)
+    VALUES
+        (
+         '".$prdNombre."', 
+         ".$prdPrecio.",
+         ".$idMarca.",
+         ".$idCategoria.",
+         '".$prdDescripcion."',
+         '".$prdImagen."'
+         )";
+    $link = conectar();
+    try{
+        $resultado = mysqli_query($link, $sql);
+        $_SESSION['mensaje'] = 'Producto: ' .$prdNombre. ' agregado correctamente';
+        $_SESSION['css'] = 'success';
+        // redireccion a panel adminMarcas con mensaje ok
+        header('location: adminProductos.php');
+        return $resultado;
+    }
+    catch( Exception $e ){
+        // log de errores  logErrores($e)
+        $_SESSION['mensaje'] = 'No se pudo agregar producto' .$prdNombre;
+        $_SESSION['css'] = 'danger';
+        header('location: adminProductos.php');
+        return false;
+    }
+
+}
+
 /**
  * listarProductos()
  * verProductoPorID()
