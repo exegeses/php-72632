d25149473ef1bf041384d6826d6cc0a1ddfffbe2
Marcos <exegeses@gmail.com>
2024-12-28 13:05:42 -0300

    teoría de autenticación

---
+++ curso/catalogo/funciones/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+    function login()
+    {}
+
+    function logout()
+    {}
+
+/**
+ * función para chequear que el usuario esté logueado
+ * @return void
+ */
+    function auth()
+    {}
+
+/**
+ * función para chequear que el usuario sea administrador
+ * @return void
+ */
+    function checkAdmin()
+    {}
